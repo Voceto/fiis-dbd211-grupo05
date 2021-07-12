f7838731491f9f11dad51cffafd75f6d7476fb81
Victor Contreras <victor.contreras.c@uni.pe>
2021-07-12 02:59:21 -0500

    inicio sesion

--- pc04/src/main/resources/static/js/script_actividades.js
+++ pc04/src/main/resources/static/js/script_actividades.js
@@ -1,25 +1,27 @@
-
- const app = new Vue({
+const app = new Vue({
     el:'#actividades',
     data:{
-        idProyecto:'B&PS-PAC-PC-0002',
+        idProyecto:localStorage.getItem("cod_proy"),
         actividades:[],
         listaBand:true,
         actividadBand:false,
-        actividad:{},
+        actividad:null,
     },
     methods:{
+        verLista:function(){
+            this.actividadBand=false;
+            this.listaBand=true;
+            axios.post('/listaActividades',{ id: this.idProyecto }).then(response => (this.actividades = response.data));
+        },
         verActividad:function(p1,p2){
             this.actividadBand=true;
             this.listaBand=false;
-            axios.post('/obtenerActividad',{codigo:p1,codigo_tipo:p2}).then(response => (this.actividad=response.data))
-        },
-        volverLista:function(){
-            this.actividadBand=false;
-            this.listaBand=true;
+            axios.post('/obtenerActividad',{codigo:p1,codigo_tipo:p2}).then(response => (this.actividad=response.data));
+            localStorage.setItem("cod_act",p1);
+            localStorage.setItem("cod_tipo_act",p2);
         }
     },
     mounted () {
-        axios.post('/listaActividades',{ id: this.idProyecto }).then(response => (this.actividades = response.data))
+        this.verLista();
     },
 })
--- pc04/src/main/resources/static/js/script_proyectos.js
+++ pc04/src/main/resources/static/js/script_proyectos.js
@@ -1,7 +1,8 @@
+localStorage.setItem("usuario","erzehorj")
 const app = new Vue({
     el:'#proyectos',
     data:{
-        usename:localStorage.getItem("usuario"),
+        username:localStorage.getItem("usuario"),
         ultProyBand:true,
         listaBand:false,
         proyBand:false,
@@ -13,14 +14,25 @@ const app = new Vue({
         verUltimoProy:function(){
             this.ultProyBand=true;
             this.listaBand=false;
-            axios.post('/obtenerActividad',{codigo:p1,codigo_tipo:p2}).then(response => (this.actividad=response.data))
+            this.proyBand=false;
+            axios.post('/obtenerUltimoProyecto',{id:this.username}).then(response => (this.ultProyBand=response.data))
         },
-        volverLista:function(){
+        verLista:function(){
             this.actividadBand=false;
             this.listaBand=true;
+            this.proyBand=false;
+            axios.post('/listaProyectos',{id:this.username}).then(response=>(this.proyectos=response.data))
+        },
+        verProyecto:function(cod){
+            this.actividadBand=false;
+            this.listaBand=false;
+            this.proyBand=true;
+            axios.post('/obtenerProyecto',{id:cod}).then(response=>(this.proyecto=response.data));
+            localStorage.setItem("cod_proy",cod)
         }
+
     },
     mounted () {
-        this.verUltimoProy;
+            this.verLista();
     },
 })
